a2cf929b67775b3fc918eb5cb0438ffa4e79b541
yoenarindu <yoenarindu@gmail.com>
2022-03-11 14:55:03 +0700

    Menambahkan file variable.php

---
+++ variable.php
@@ -0,0 +1,22 @@
+<?php
+    $a = 20;
+    $b = 5;
+    $c = 80;
+
+    $penjumlahan = $a + $b;
+    $pengurangan = $c - $a;
+    $perkalian = $c * $b;
+    $pembagian = $c / $a;
+
+    $hasil = $penjumlahan + $pengurangan * $pembagian;
+
+    echo "Hasil penjumlahan = $penjumlahan";
+    echo "<br/>";
+    echo "Hasil pengurangan = $pengurangan";
+    echo "<br/>";
+    echo "Hasil perkalian = $perkalian";
+    echo "<br/>";
+    echo "Hasil pembagian = $pembagian";
+    echo "<br/>";
+    echo "Hasil dari variabel Hasil = $hasil";
+?>
